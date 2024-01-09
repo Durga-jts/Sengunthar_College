e9aad5073e14ed703b4ea9fbe37ec2fcfa59574f
Durga-jts <durga.jorimts@gmail.com>
2024-01-09 18:21:06 +0530

    Jan 09

---
+++ about-us.php
@@ -0,0 +1,304 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Senghundhar College of Pharmacy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
+       <!-- all css here -->
+       <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/slicknav.min.css">
+    <link rel="stylesheet" href="assets/css/typography.css">
+    <link rel="stylesheet" href="assets/css/default-css.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+    <!--color css-->
+    <link rel="stylesheet" id="triggerColor" href="assets/css/triggerplate/color-0.css">
+    <!-- modernizr css -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+<!-- /* Header */ -->
+<?php include("header.php"); ?>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- prealoader area end -->
+    
+    <!-- offset search area start -->
+    <div class="offset-search">
+        <form action="#">
+            <input type="text" name="search" placeholder="Search here...">
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+    </div>
+    <!-- offset search area end -->
+    <!-- body overlay area start -->
+    <div class="body_overlay"></div>
+    <!-- body overlay area end -->
+    <!-- crumbs area start -->
+    <div class="crumbs-area">
+        <div class="container">
+            <div class="crumb-content">
+                <h4 class="crumb-title"><span>About </span>Us</h4>
+            </div>
+        </div>
+    </div>
+    <!-- crumbs area end --> 
+    <!-- about area start -->
+    <div class="about-area ptb--120">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="about-left-content">
+                        <div class="section-title">
+                             <span class="text-uppercase">about us</span>
+                            <h2>Welcome to</h2><h2><span class="primary-color">Senghundhar College of Pharmacy</span></h2> 
+                        </div>
+                        <p>Established in 2017, Senghundhar College of Pharmacy is dedicated to achieving excellence in Pharmacy Education. Affiliated with the prestigious “The Tamil Nadu Dr. MGR Medical University” in Chennai and approved by the Pharmacy Council of India (PCI), New Delhi, under section 12 of the Pharmacy Act, our institution aims to redefine education, particularly for rural students in India.
+                            Our college creates an environment that instills a strong work ethic, discipline, and a set of principles that will empower students for their future studies and careers. We take pride in offering enhanced hands-on training, well-established infrastructure, a well-resourced library, industrial visits, guest lectures, workshops, symposiums, conferences, and student exhibitions.
+                            </p>
+                        
+                        <!-- <a href="#" class="btn btn-primary btn-round">Read more</a> -->
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="abt-right-thumb">
+                        <div class="abt-rt-inner">
+                            <a class="expand-video" href="https://www.youtube.com/watch?v=YZWdIG7K8fE"><i class="fa fa-play"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- about area end -->
+
+ 
+<!-- Vision and Mission area start -->
+<div class="vision-mission-area ptb--120">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="vision-content">
+                    <div class="section-title">
+                        <span class="text-uppercase">vision & mission</span>
+                        <h2>Our Vision</h2>
+                    </div>
+                    <p>Our vision is to be recognized as one of the premier institutes in Pharmacy Education, Research, and Pharmacy Practice at both the National and International levels. We strive to create an educational ecosystem that not only imparts knowledge but also nurtures a passion for continuous learning and innovation.
+                        At Senghundhar College of Pharmacy, our commitment to the growth and success of our students is unwavering. The constant reviews, personalized attention, and timely counseling provided by our management, principal, and faculty members ensure that each student has the support needed to overcome challenges and achieve their highest potential. Our institution takes pride in producing graduates who, like inestimable buds, spread fragrance throughout the pharmaceutical world.
+                        </p>
+
+                    <div class="section-title mt-4">
+                        <h2>Our Mission</h2>
+                    </div>
+                    <p>Our mission at Senghundhar College of Pharmacy is multi-faceted:
+                        Pharma Education and Talent Development:
+                        We are committed to providing pharmaceutical education and unlocking the hidden talents of students from rural backgrounds, recognizing the vital role they play in our nation's development.
+                        Innovative Teaching Methods:
+                        We employ innovative, practical-oriented teaching methods to ensure a comprehensive learning experience, fostering an atmosphere conducive to academic and professional growth.
+                        Cutting-edge Research:
+                        Our mission includes designing cutting-edge research in the field of pharmaceutical sciences, drug development, and clinical pharmacy, contributing to advancements in the pharmaceutical industry.
+                        Intellectual Property Development:
+                        We actively promote and support both national and international intellectual property development, encouraging students to engage in creative and innovative endeavors.
+                        Entrepreneurial Spirit and Ethical Standards:
+                        We foster an entrepreneurial spirit among our students, encouraging them to explore entrepreneurial opportunities in the pharmaceutical sector. Additionally, we prioritize the inculcation of high moral, ethical, and professional standards, shaping responsible and principled professionals.
+                        </p>
+                </div>
+            </div>
+            <!-- Add any additional content or images for Vision and Mission -->
+        </div>
+    </div>
+</div>
+<!-- Vision and Mission area end -->
+
+    <!-- teacher area start -->
+    <div class="teacher-area pb--70 mt-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-5 col-md-6">
+                    <div class="section-title">
+                        <span class="text-uppercase">Learn from best</span>
+                        <h2>Our Professors</h2> 
+                    </div>
+                </div>
+            </div>
+            <div class="commn-carousel owl-carousel card-deck">   
+                <div class="card mb-5" style="height:600px;"> 
+                    <img src="assets/images/professor2.jpg" alt="image"> 
+                    <div class="card-body teacher-content p-25">  
+                        <h4 class="card-title mb-4">Rajesh Kumar</h4>
+                        <span class="primary-color font-italic d-block mb-3">Senior Professor</span>
+                        <br>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-github"></i></a></li>
+                        </ul> 
+                    </div>
+                </div><!-- card -->    
+
+              <div class="card mb-5" style="height:600px;"> 
+                <img src="assets/images/professor3.jpg" alt="image"> 
+                <div class="card-body teacher-content p-25">  
+                  <h4 class="card-title mb-4">Sharmila</h4>
+                  <span class="primary-color font-italic d-block mb-3">Assistant Professor</span>
+                 <br>
+                  <ul class="list-inline">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github"></i></a></li>
+                  </ul>
+                </div>
+              </div><!-- card -->    
+
+              <div class="card mb-5" style="height:600px;"> 
+                <img src="assets/images/professor4.jpg" alt="image"> 
+                <div class="card-body teacher-content p-25">  
+                  <h4 class="card-title mb-4">Bindu</h4>
+                  <span class="primary-color font-italic d-block mb-3">Professor</span>
+                 <br>
+                  <ul class="list-inline">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github"></i></a></li>
+                  </ul>
+                </div>
+              </div><!-- card -->  
+              <div class="card mb-5" style="height:600px;"> 
+                <img src="assets/images/professor1.jpg" alt="image"> 
+                <div class="card-body teacher-content p-25">  
+                  <h4 class="card-title mb-4">Vijay</h4>
+                  <span class="primary-color font-italic d-block mb-3">Associate Professor</span>
+                  <br>
+                  <ul class="list-inline">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                  </ul>
+                </div>
+              </div><!-- card -->         
+            </div>
+        </div>
+    </div>
+    <!-- teacher area end -->
+ 
+     <!-- cta area start --> 
+     <div class="cta-area primary-bg has-color ptb--50"> 
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-9">
+                    <div class="cta-content">
+                        <p class="mb-2">Click to Join the Advance Workshop</p>
+                        <h2>Training in advance networking</h2>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="cta-btn">
+                        <a class="btn btn-light btn-round" href="#">Join Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- cta area end -->
+
+    <!-- footer area start -->
+    <footer>
+        <div class="footer-top has-color pt--50 pb--30">
+            <div class="container-fluid">
+                <div class="row ml-5">
+                    <div class="col-md-4">
+                        <div class="widget widget-company">
+                            <a href="index2.html" style="width:40%;margin-left:-2%;"><img src="assets/images/logo2.png" alt="image"></a>
+                            <div class="addr" style="margin-top:-30px;">
+                                <div class="address">
+                                    <h6>COLLEGE ADDRESS</h6>
+                                    <p>No.129, RedHills, By Pass Road, Chennai, Tamil Nadu.</p>
+                                </div>
+                                <div class="address">
+                                    <h6>PHONE NUMBER</h6>
+                                    <p>+91 9677192226</p>
+                                </div>
+                                <div class="address">
+                                    <h6>EMAIL ADDRESS</h6>
+                                    <p>srikumaranhealthcentre@gmail.com</p>
+                                    <p>srikumaran_hc@gmail.com</p>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="widget footer-link">
+                            <h4 class="fwidget-title mb-5 pb-3 primary-color">Information</h4> 
+                            <ul>
+                                <li><a href="courses.html"><i class="fa fa-arrow-right"></i>Case Studies</a></li>
+                                <li><a href="about.html"><i class="fa fa-arrow-right"></i>About Us</a></li>
+                                <li><a href=""><i class="fa fa-arrow-right"></i>Our Story</a></li>
+                                <li><a href="blog.html"><i class="fa fa-arrow-right"></i>Blog Post</a></li>
+                                <li><a href="teachers.html"><i class="fa fa-arrow-right"></i>Work with Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="widget widget-opening">
+                            <h4 class="fwidget-title mb-5 pb-3 primary-color">Working Day & time</h4>
+                            
+                            <p style="width:75%;">The college observes holidays on government-designated days and operates during regular working days.</p>
+                           <ul>
+                                <li><span>Mon - Tus :</span>6.00 am - 10.00 pm</li>
+                                <li><span>Wed - Thu :</span>8.00 am - 6.00 pm</li>
+                                <li><span>Friday :</span>3.00 pm - 8.00 pm</li>
+                                <li><span>Sunday :</span>Closed</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <ul class="social">
+                        <li><a href="https://www.facebook.com/retterikumaranhospital"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://twitter.com/i/flow/login?redirect_after_login=%2FRetterikumaran1"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+                <div class="footer-bottom">
+                    <p>Copyright © 2022 <span><a class="primary-color" href="" target="_blank"></a> </span> - All Rights Reserved. Made by <span><a class="primary-color" href="https://jorim.in/" target="_blank">Jorim Technology Solution</a></span></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer area end -->
+ 
+    <!-- jquery latest version -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- others plugins -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,103 @@
+   <!-- BOOTSTRAP -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
+    <!-- all css here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/slicknav.min.css">
+    <link rel="stylesheet" href="assets/css/typography.css">
+    <link rel="stylesheet" href="assets/css/default-css.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!--color css-->
+    <link rel="stylesheet" id="triggerColor" href="assets/css/triggerplate/color-0.css">
+    <!-- modernizr css -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+   
+   <!-- header area start -->
+   <header id="header">
+        <!-- header top area start -->
+        <div class="header-top">
+            <div class="container-fluid">
+                <div class="row d-flex flex-center">
+                    <div class="col-sm-8">
+                        <div class="ht-address ml-5">
+                            <ul>
+                                <li><i class="fa fa-phone"></i>+91 9367653355 / 8825707617</li>
+                                <li><i class="fa fa-envelope"></i>info@senguntharpharmacycollege.org </li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="ht-social mr-1">
+                            <ul>
+                                <li><a href="https://www.facebook.com/SenghundharCollegeofPharmacy"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://twitter.com/scop_2017 "><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="https://www.youtube.com/@SCOP_2017 "><i class="fa fa-youtube"></i></a></li>
+                                <li><a href="https://www.instagram.com/senghundharcollegeofpharmacy/"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- header top area end -->
+        <!-- header bottom area start -->
+        <div class="header-bottom">
+            <div class="container-fluid">
+                <div class="header-bottom-inner">
+                    <div class="row align-items-center">
+                        <div class="col-md-1 col-sm-9">
+                            <div class="logo">
+                                <a href="index.html" style="width:145px;"><img src="assets/images/logo1.png" alt="logo"></a>
+                            </div>
+                        </div>
+                        <div class="col-md-9 d-none d-lg-block">
+                            <div class="main-menu">
+                                <nav>
+                                    <ul id="m_menu_active">
+                                        <li class="active"><a href="index.html">Home</a></li>
+                                        <li><a href="about-us.html">About College</a></li>
+                                        <li><a href="#">Academic </a></li>
+                                        <li><a href="#">Facilities </a></li>
+                                        <li><a href="#">Admission </a>
+                                            <!-- <ul class="submenu">
+                                                <li><a href="courses.html">courses</a></li>
+                                                <li><a href="course-details.html">course details</a></li>
+                                            </ul> -->
+                                        </li>
+                                        <li><a href="#">Gallery </a>
+                                            <!-- <ul class="submenu">
+                                                <li><a href="blog.html">blog</a></li>
+                                                <li><a href="blog-details.html">blog details</a></li>
+                                            </ul> -->
+                                        </li>
+                                        <li><a href="#">Careers </a></li>
+                                        <li><a href="#">Contact us </a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="col-md-1 logo">
+                            <a href="index.html" style="width:145px;"><img src="assets/images/vaarahi-logo.jpeg" alt="logo" class="ml-4"></a>
+                        </div>
+                        <div class="col-md-1 col-sm-3">
+                            <div class="hb-right w-25">
+                                <ul> 
+                                    <li class="search_btn"><i class="fa fa-search"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- <div class="col-12 d-block d-lg-none">
+                            <div id="mobile_menu"></div>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- header bottom area end -->
+    </header>
